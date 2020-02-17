de51eaeb7d229dc834b9a561458e75d8253df1b5
abdulhasif <abdulhasif99@gmail.com>
2020-02-17 18:02:29 +0530

    signup page validation

---
+++ app/src/main/java/com/ajgroups/popas/DataAdapter/UserAdapter.java
@@ -0,0 +1,96 @@
+package com.ajgroups.popas.DataAdapter;
+
+public class UserAdapter {
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private String age;
+    private String gender;
+    private String bloodGroup;
+    private String weight;
+    private String height;
+    private String mobileNumber;
+    private String password;
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBloodGroup() {
+        return bloodGroup;
+    }
+
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
--- app/src/main/java/com/ajgroups/popas/SignUp.java
+++ app/src/main/java/com/ajgroups/popas/SignUp.java
@@ -10,9 +10,14 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.ajgroups.popas.DataAdapter.UserAdapter;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
@@ -23,13 +28,27 @@ import java.util.ArrayList;
 public class SignUp extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private Spinner bloodGroup;
     private Button register;
+    private String bloodG;
+    private EditText userName,firstName,lastName,age,weight,height,mobile,password,confirmPassword;
     private String bloodGroupList[] = {"A+ve","A-ve","B+ve","B-ve","AB+ve","AB-ve","O+ve","O-ve"};
+    private RadioButton male,female;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         bloodGroup = findViewById(R.id.bloodGroup);
         register = findViewById(R.id.register);
+        userName = findViewById(R.id.userName);
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+        age = findViewById(R.id.age);
+        weight = findViewById(R.id.weight);
+        height = findViewById(R.id.height);
+        mobile = findViewById(R.id.mobile);
+        password = findViewById(R.id.password);
+        male = findViewById(R.id.male);
+        female = findViewById(R.id.female);
+        confirmPassword = findViewById(R.id.confirmPassword);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,bloodGroupList);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         bloodGroup.setAdapter(adapter);
@@ -37,6 +56,38 @@ public class SignUp extends AppCompatActivity implements AdapterView.OnItemSelec
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String pass, cpass;
+                pass = password.getText().toString();
+                cpass= confirmPassword.getText().toString();
+                UserAdapter users = new UserAdapter();
+                if(male.isChecked())
+                {
+                    users.setGender("Male");
+                }
+                else if(female.isChecked())
+                {
+                    users.setGender("Female");
+                }
+                users.setUserName(userName.getText().toString());
+                users.setFirstName(firstName.getText().toString());
+                users.setLastName(lastName.getText().toString());
+                users.setAge(age.getText().toString());
+                users.setWeight(weight.getText().toString());
+                users.setHeight(height.getText().toString());
+                users.setMobileNumber( mobile.getText().toString());
+                users.setBloodGroup(bloodG);
+                if(users.getUserName().isEmpty() || users.getFirstName().isEmpty() || users.getLastName().isEmpty() || users.getAge().isEmpty() ||  users.getWeight().isEmpty() || users.getHeight().isEmpty() || users.getMobileNumber().isEmpty() || pass.isEmpty() || cpass.isEmpty() || bloodG.isEmpty())
+                {
+                    Snackbar.make(view,"Please fill all the fields",Snackbar.LENGTH_LONG).show();
+                    return;
+                }
+                if(pass.equals(cpass)){
+                    users.setPassword(pass);
+                }else{
+                    Snackbar.make(view,"Please enter same password in both password and confirm password",Snackbar.LENGTH_LONG).show();
+                    return;
+                }
+
 
             }
         });
@@ -64,7 +115,7 @@ public class SignUp extends AppCompatActivity implements AdapterView.OnItemSelec
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
+            bloodG = bloodGroup.getSelectedItem().toString();
     }
 
     @Override
